269f6492170efbf3abb43d1fd679aaa7eceb718b
Hassan Abedi <hassan.abedi.t@gmail.com>
2018-08-30 13:06:42 +0430

    improved management script

---
+++ manage.sh
@@ -0,0 +1,27 @@
+app_name="src.main:app"
+cwd=`pwd`
+
+case $1 in
+    gunicorn-start)
+	host_port="localhost:81"
+	num_workerz=10
+	gunicorn $app_name -b $host_port --chdir $cwd --workers $num_workerz --daemon
+	;;
+    gunicorn-stop)
+	kill -9 `ps aux | grep gunicorm | grep $app_name | awk '{ print $2 }' `
+	;;
+    nginx-start)
+	nginx -c "$cwd/src/resource/nginx.conf"
+	;;
+    nginx-stop)
+	nginx -s stop
+	;;
+    redis-start)
+	redis-server "$cwd/src/resource/redis.conf"
+	;;
+    redis-stop)
+	redis-cli shutdown
+	;;
+    *)
+	;;
+esac
